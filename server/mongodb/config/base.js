0b3a669a9f51f7ee878750b360ec9d1581625032
linengine <linengine@gmail.com>
2018-03-29 10:58:27 +0800

    update server file

---
+++ server/mongodb/config/base.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'mongodb init',
+  dbname:
+    'mongodb://<dbuser>:<dbpassword>@ds117539.mlab.com:17539/pttjobsniper',
+  dbuser: 'linengine',
+  dbpassword: 'mongodb54682',
+  secret: 'my_secret_key',
+};
